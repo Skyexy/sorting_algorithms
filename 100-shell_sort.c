916571c18b2208c0123b224cbfb9e6e179131627
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-07-19 20:53:28 +0100

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,24 +11,24 @@
  */
 void shell_sort(int *array, size_t size)
 {
-    int temp;
-    int gap = 1;
-    int i;
-    int num = size;
-    int j;
+	int temp;
+	int gap = 1;
+	int i;
+	int num = size;
+	int j;
 
-    for (gap = gap * 3 + 1; gap > 0; gap /= 3)
-    {
-        for (i = gap; i < num; i++)
-        {
-            temp = array[i];
-
-            for (j = i; j >= gap && array[j - gap] > temp; j -=gap)
-            {
-                array[j] = array[j - gap];
-            }
-            array[j] = temp;
-        }
-        print_array(array, size);
-    }
+	for (gap = gap * 3 + 1; gap > 0; gap /= 3)
+	{
+		for (i = gap; i < num; i++)
+		{
+			temp = array[i];
+			
+			for (j = i; j >= gap && array[j - gap] > temp; j -=gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = temp;
+		}
+		print_array(array, size);
+	}
 }
