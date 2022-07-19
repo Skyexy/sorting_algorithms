5215fe6f17a9981d9879b0836d8daa6c459162ad
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-07-19 21:09:23 +0100

    Update 103-merge_sort.c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -14,48 +14,47 @@
  */
 void merge_elements(int *arr, int front, int tail, int middle)
 {
-    int s1 = (middle - front) + 1;
-    int s2 = (tail - middle);
-    int *firstsubarray;
-    int *secondsubarray;
-    int i;
-    int j;
-    int counter = 0;
+	int s1 = (middle - front) + 1;
+	int s2 = (tail - middle);
+	int *firstsubarray;
+	int *secondsubarray;
+	int i;
+	int j;
+	int counter = 0;
 
-    firstsubarray = malloc(sizeof(int) * s1);
-    secondsubarray = malloc(sizeof(int) * s2);
-    for (i = 0; i < s1; i++)
-    {
-        firstsubarray[i] = arr[front + i];
-    }
-    for (i = 0; i < s2; i++)
-    {
-        secondsubarray[i] = arr[middle + i + 1];
-    }
-    printf("Merging...\n");
-    printf("[left]: ");
-    print_array(firstsubarray, s1);
-    printf("[right]: ");
-    print_array(secondsubarray, s2);
-    i = 0;
-    j = 0;
-    while (counter < (s1 + s2))
-    {
-        if (i < s1 && j < s2)
-        {
-            if (firstsubarray[i] <= secondsubarray[j])
-            {
-                arr[front + counter] = firstsubarray[i];
-                i++;
-            }
-            else
-            {
-                arr[front + counter] = secondsubarray[j];
-                j++;
-            }
-
-        }
-        else if (i < s1)
+	firstsubarray = malloc(sizeof(int) * s1);
+	secondsubarray = malloc(sizeof(int) * s2);
+	for (i = 0; i < s1; i++)
+	{
+		firstsubarray[i] = arr[front + i];
+	}
+	for (i = 0; i < s2; i++)
+	{
+		secondsubarray[i] = arr[middle + i + 1];
+	}
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(firstsubarray, s1);
+	printf("[right]: ");
+	print_array(secondsubarray, s2);
+	i = 0;
+	j = 0;
+	while (counter < (s1 + s2))
+	{
+		if (i < s1 && j < s2)
+		{
+			if (firstsubarray[i] <= secondsubarray[j])
+			{
+				arr[front + counter] = firstsubarray[i];
+				i++;
+			}
+			else
+			{
+				arr[front + counter] = secondsubarray[j];
+				j++;
+			}
+		}
+		else if (i < s1)
 		{
 			arr[front + counter] = firstsubarray[i];
 			i++;
@@ -65,10 +64,10 @@ void merge_elements(int *arr, int front, int tail, int middle)
 			arr[front + counter] = secondsubarray[j];
 			j++;
 		}
-        counter++;
-    }
-    printf("[Done]: ");
-    print_array(arr, s1 + s2);
+		counter++;
+	}
+	printf("[Done]: ");
+	print_array(arr, s1 + s2);
 }
 
 /**
@@ -82,17 +81,17 @@ void merge_elements(int *arr, int front, int tail, int middle)
  */
 void sort_element(int *arr, int front, int tail)
 {
-    int m;
+	int m;
 
-    if (front < tail)
-    {
-        m = (front + tail) / 2;
+	if (front < tail)
+	{
+		m = (front + tail) / 2;
 
-        sort_element(arr, front, m);
-        sort_element(arr, m + 1, tail);
+		sort_element(arr, front, m);
+		sort_element(arr, m + 1, tail);
 
-        merge_elements(arr, front, tail + 1, m);
-    }
+		merge_elements(arr, front, tail + 1, m);
+	}
 }
 
 /**
@@ -104,5 +103,5 @@ void sort_element(int *arr, int front, int tail)
  */
 void merge_sort(int *array, size_t size)
 {
-    sort_element(array, 0, size - 1);
+	sort_element(array, 0, size - 1);
 }
