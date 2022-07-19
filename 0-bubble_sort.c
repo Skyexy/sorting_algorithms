ffc7fff5c8a9454ac60b2fc846560043eb105744
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-07-19 20:48:15 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,33 +12,21 @@
 
 void bubble_sort(int *array, size_t size __attribute__((unused)))
 {
-    size_t i;
-    size_t j;
-    int temp;
+	size_t i;
+	size_t j;
+	int temp;
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < (size - 1); j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j + 1];
-                array[j + 1] = array[j];
-                array[j] = temp;
-                print_array(array, size);
-            }
-        }
-    }
-}
-int main(void)
-{
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-
-    print_array(array, n);
-    printf("\n");
-    bubble_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
